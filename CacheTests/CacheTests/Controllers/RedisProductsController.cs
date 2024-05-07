fd275371afba3aa8ae7f3987c30de4fc9400555f
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-07 14:20:11 +0300

    redis get,set,remove eklendi

--- CacheTests/CacheTests/Controllers/RedisProductsController.cs
+++ CacheTests/CacheTests/Controllers/RedisProductsController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CacheTests.Entity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CacheTests.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class RedisProductsController : ControllerBase
     {
@@ -13,6 +15,35 @@ namespace CacheTests.Controllers
         public RedisProductsController(IDistributedCache distributedCache)
         {
             _distributedCache = distributedCache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SetRedisData()
+        {
+            DistributedCacheEntryOptions distributedCacheEntryOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(100)
+            };
+
+            await _distributedCache.SetStringAsync("name", "Computer", distributedCacheEntryOptions);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetRedisData()
+        {
+            string? productName = await _distributedCache.GetStringAsync("name");
+
+            return Ok(productName);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> RemoveRedisData()
+        {
+            await _distributedCache.RemoveAsync("name");
+
+            return Ok();
         }
     }
 }
