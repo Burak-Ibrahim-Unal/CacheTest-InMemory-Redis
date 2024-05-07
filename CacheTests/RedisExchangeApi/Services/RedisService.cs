781067d8a481d3c39c17738e99793ad88f220655
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-07 18:34:24 +0300

    redis api

---
+++ CacheTests/RedisExchangeApi/Services/RedisService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisExchangeApi.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly string _redisPort;
+        private ConnectionMultiplexer _redisMultiplexer;
+        public IDatabase database { get; set; }
+
+        public RedisService(IConfiguration configuration)
+        {
+            _redisHost = configuration["Redis:Host"];
+            _redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{_redisHost}:{_redisPort}";
+            _redisMultiplexer = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redisMultiplexer.GetDatabase(db);
+        }
+    }
+}
