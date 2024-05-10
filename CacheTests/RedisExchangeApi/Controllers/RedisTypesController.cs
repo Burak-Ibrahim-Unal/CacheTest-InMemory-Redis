85ae7d8aaa18ef7c6f0a91c18f184ad608ee1492
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-10 17:45:27 +0300

    redis set

--- CacheTests/RedisExchangeApi/Controllers/RedisTypesController.cs
+++ CacheTests/RedisExchangeApi/Controllers/RedisTypesController.cs
@@ -12,13 +12,16 @@ namespace RedisExchangeApi.Controllers
         private readonly RedisService _redisService;
         private readonly IDatabase _db;
         private readonly IDatabase _db2;
+        private readonly IDatabase _db3;
         public string redisListText = "names";
+        public string redisListText2 = "hashnames";
 
         public RedisTypesController(RedisService redisService)
         {
             _redisService = redisService;
             _db = _redisService.GetDb(0);
             _db2 = _redisService.GetDb(1);
+            _db3 = _redisService.GetDb(2);
         }
 
         [HttpPost]
@@ -72,7 +75,40 @@ namespace RedisExchangeApi.Controllers
         [HttpPost]
         public ActionResult RemoveRedisListItem(string name)
         {
-            _db2.ListRemove(redisListText,name);
+            _db2.ListRemove(redisListText, name);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult SetRedisSetValuesDb(string name) // unique values only
+        {
+            _db3.KeyExpire(redisListText2, DateTime.Now.AddMinutes(5));
+            _db3.SetAdd(redisListText2, name);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult GetRedisSetValuesDb()
+        {
+            HashSet<string> redisHashSetList = new HashSet<string>();
+
+            if (_db3.KeyExists(redisListText2))
+            {
+                _db3.SetMembers(redisListText2).ToList().ForEach(x =>
+                {
+                    redisHashSetList.Add(x);
+                });
+            }
+
+            return Ok(redisHashSetList);
+        }
+
+        [HttpPost]
+        public ActionResult RemoveRedisSetItem(string name)
+        {
+            _db3.SetRemove(redisListText2, name);
 
             return Ok();
         }
