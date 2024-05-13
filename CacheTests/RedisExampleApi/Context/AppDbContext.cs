c083c757ce02ab92cbd1e1b152b0c069ec4d74c0
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-13 20:29:24 +0300

    db context updated,repositories added,services implemented

--- CacheTests/RedisExampleApi/Context/AppDbContext.cs
+++ CacheTests/RedisExampleApi/Context/AppDbContext.cs
@@ -11,5 +11,21 @@ namespace RedisExampleApi.Context
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product {Id=1,Name="TestProduct1",Price=10 },
+                new Product {Id=2,Name="TestProduct2",Price=11 },
+                new Product {Id=3,Name="TestProduct3",Price=12 },
+                new Product {Id=4,Name="TestProduct4",Price=13 },
+                new Product {Id=5,Name="TestProduct5",Price=14 },
+                new Product {Id=6,Name="TestProduct6",Price=15 },
+                new Product {Id=7,Name="TestProduct7",Price=16 },
+                new Product {Id=8,Name="TestProduct8",Price= 17 }
+        );
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- CacheTests/RedisExampleApi/Program.cs
+++ CacheTests/RedisExampleApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RedisExampleApi.Context;
+using RedisExampleApi.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase("TestDatabase");
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbcontext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbcontext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
