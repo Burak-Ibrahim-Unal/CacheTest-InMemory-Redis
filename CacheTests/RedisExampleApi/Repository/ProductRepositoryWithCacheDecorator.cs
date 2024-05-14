bc0992a2b7be489c1a3afdca2796012ef9c4105c
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-05-14 11:51:29 +0300

    decorator pattern updated

---
+++ CacheTests/RedisExampleApi/Repository/ProductRepositoryWithCacheDecorator.cs
@@ -0,0 +1,74 @@
+﻿using RedisExample.Cache;
+using RedisExampleApi.Models;
+using StackExchange.Redis;
+using System.Text.Json;
+
+namespace RedisExampleApi.Repository
+{
+    public class ProductRepositoryWithCacheDecorator : IProductRepository
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly RedisRepository _redisService;
+        private readonly IDatabase _redisDb;
+        private const string redisProductKey = "productsCache";
+
+        public ProductRepositoryWithCacheDecorator(RedisRepository redisService, IProductRepository productRepository)
+        {
+
+            _redisService = redisService;
+            _redisDb = _redisService.GetDb(2);
+            _productRepository = productRepository;
+        }
+        public async Task<Product> CreateAsync(Product product)
+        {
+            var addedProduct = await _productRepository.CreateAsync(product);
+            if (await _redisDb.KeyExistsAsync(redisProductKey))
+            {
+                await _redisDb.HashSetAsync(redisProductKey, product.Id, JsonSerializer.Serialize(addedProduct));
+            }
+
+            return addedProduct;
+        }
+
+        public async Task<List<Product>> GetAsync()
+        {
+            if (!await _redisDb.KeyExistsAsync(redisProductKey))
+                return await LoadProductsFromCache();
+
+            var productList = new List<Product>();
+            var cachedProducts = await _redisDb.HashGetAllAsync(redisProductKey);
+
+            foreach (var cachedProduct in cachedProducts.ToList())
+            {
+                var product = JsonSerializer.Deserialize<Product>(cachedProduct.Value);
+                productList.Add(product);
+            }
+
+            return productList;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            if (await _redisDb.KeyExistsAsync(redisProductKey))
+            {
+                var product = await _redisDb.HashGetAsync(redisProductKey, id);
+                return product.HasValue ? JsonSerializer.Deserialize<Product>(product) : null;
+            }
+
+            var products = await LoadProductsFromCache();
+            return products.FirstOrDefault(x => x.Id == id);
+        }
+
+        private async Task<List<Product>> LoadProductsFromCache()
+        {
+            var products = await _productRepository.GetAsync();
+
+            products.ForEach(product =>
+            {
+                _redisDb.HashSet(redisProductKey, product.Id, JsonSerializer.Serialize(product));
+            });
+
+            return products;
+        }
+    }
+}
